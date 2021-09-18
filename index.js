49d3a76ef09c38a7f6bb3c4e37650d80a9947fd1
Magesh16 <mageshsk16@gmail.com>
2021-09-18 12:38:41 +0530

    Add authentication functions

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-
+const authRoutes = require("./routes/auth");
 require('dotenv').config();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
-
+app.use('/auth',authRoutes);
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
