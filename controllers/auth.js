5eb7dd74fafec065976e2c5b7e01c28e93d16993
Magesh16 <mageshsk16@gmail.com>
2021-09-28 23:05:23 +0530

    Refactor final code

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,11 +3,12 @@ const bcrypt = require('bcrypt');
 const StreamChat = require('stream-chat').StreamChat;
 const crypto = require('crypto');
 
-
 require('dotenv').config();
+
 const api_key = process.env.STREAM_API_KEY;
 const api_secret = process.env.STREAM_API_SECRET;
 const app_id = process.env.STREAM_APP_ID;
+
 const signup = async (req, res) => {
     try {
         const { fullName, username, password, phoneNumber } = req.body;
@@ -28,28 +29,31 @@ const signup = async (req, res) => {
     }
 };
 
-const login = async() => {
-    try{
-        const {username,password} = req.body;
-        const serverClient = connect(api_key, api_secret,app_id);
-        const client =StreamChat.getInstance(api_key,api_secret);
+const login = async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        
+        const serverClient = connect(api_key, api_secret, app_id);
+        const client = StreamChat.getInstance(api_key, api_secret);
 
-        const {users} = await client.queryUsers({name : username});
+        const { users } = await client.queryUsers({ name: username });
 
-        if(!users) return res.status(400).json({message: "user not found"});
+        if(!users.length) return res.status(400).json({ message: 'User not found' });
+
         const success = await bcrypt.compare(password, users[0].hashedPassword);
+
         const token = serverClient.createUserToken(users[0].id);
 
-        if(success){
-            res.status(200).json({token, fullName : users[0].fullName, username, userId : users[0].userId });
-        }else{
-            res.status(500).json({message:"Incorrect Email/Password"});
+        if(success) {
+            res.status(200).json({ token, fullName: users[0].fullName, username, userId: users[0].id});
+        } else {
+            res.status(500).json({ message: 'Incorrect password' });
         }
-    }catch{
+    } catch (error) {ads
         console.log(error);
-        res.status(500).json({message : error})
+
+        res.status(500).json({ message: error });
     }
-}
+};
 
-
-module.exports = {signup,login}
+module.exports = { signup, login }
